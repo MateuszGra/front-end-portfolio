9549fbdeb8de95c1e339a4d9ce1275018d947150
MateuszGra <mxgrabowski@gmail.com>
2019-04-03 10:10:54 +0200

    just start

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 (function() {
-      
+    const about = document.querySelector(".about");
+    const projects = document.querySelector(".projects");
+    const contact = document.querySelector(".contact");
+
+
+    const aboutSection = document.querySelector(".aboutSection");
+    const projectsSection = document.querySelector(".projectsSection");
+    const contactSection = document.querySelector(".contactSection");
+
+    about.addEventListener("click", function(e) {  
+        aboutSection.classList.remove('none');
+        projectsSection.classList.add('none');
+        contactSection.classList.add('none');
+    },false);
+
+    projects.addEventListener("click", function(e) {  
+        aboutSection.classList.add('none');
+        projectsSection.classList.remove('none');
+        contactSection.classList.add('none');
+    },false);
+
+    contact.addEventListener("click", function(e) {  
+        aboutSection.classList.add('none');
+        projectsSection.classList.add('none');
+        contactSection.classList.remove('none');
+    },false);
 })();

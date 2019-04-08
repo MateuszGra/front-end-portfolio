05ede4fbb341dc3ea25e26934493338bb7aa25b1
MateuszGra <mxgrabowski@gmail.com>
2019-04-08 10:34:00 +0200

    resize

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const about = document.querySelector(".about");
     const projects = document.querySelector(".projects");
     const contact = document.querySelector(".contact");
@@ -17,67 +17,75 @@
     const inputUrl = document.querySelector(".inputUrl");
     inputUrl.value = 'https://mateuszgra.github.io/front-end-portfolio/';
 
-    about.addEventListener("click", function(e) {  
+    about.addEventListener("click", function (e) {
         aboutSection.classList.remove('none');
         aboutSection.style.zIndex = 1;
         projectsSection.style.zIndex = 0;
         contactSection.style.zIndex = 0;
-        if(screen.width < 1000){
+        if (window.innerWidth < 1000) {
             projectsSection.classList.add('none');
             contactSection.classList.add('none');
         }
-    },false);
-    aboutSection.addEventListener("click", function(e) {  
+    }, false);
+    aboutSection.addEventListener("click", function (e) {
         aboutSection.style.zIndex = 1;
         projectsSection.style.zIndex = 0;
         contactSection.style.zIndex = 0;
-    },false);
-    projects.addEventListener("click", function(e) {  
+    }, false);
+    projects.addEventListener("click", function (e) {
         projectsSection.classList.remove('none');
         aboutSection.style.zIndex = 0;
         projectsSection.style.zIndex = 1;
         contactSection.style.zIndex = 0;
-        if(screen.width < 1000){
+        if (window.innerWidth < 1000) {
             aboutSection.classList.add('none');
             contactSection.classList.add('none');
         }
-    },false);
-    projectsSection.addEventListener("click", function(e) {  
+    }, false);
+    projectsSection.addEventListener("click", function (e) {
         aboutSection.style.zIndex = 0;
         projectsSection.style.zIndex = 1;
         contactSection.style.zIndex = 0;
-    },false);
-    contact.addEventListener("click", function(e) {  
+    }, false);
+    contact.addEventListener("click", function (e) {
         contactSection.classList.remove('none');
         aboutSection.style.zIndex = 0;
         projectsSection.style.zIndex = 0;
         contactSection.style.zIndex = 1;
-        if(screen.width < 1000){
+        if (window.innerWidth < 1000) {
             projectsSection.classList.add('none');
             aboutSection.classList.add('none');
         }
-    },false);
-    contactSection.addEventListener("click", function(e) {  
+    }, false);
+    contactSection.addEventListener("click", function (e) {
         aboutSection.style.zIndex = 0;
         projectsSection.style.zIndex = 0;
         contactSection.style.zIndex = 1;
-    },false);
+    }, false);
 
-    closeAbout.addEventListener("click", function(e) {  
+    closeAbout.addEventListener("click", function (e) {
         aboutSection.classList.add('none');
-    },false);
+    }, false);
 
-    closeProjects.addEventListener("click", function(e) {  
+    closeProjects.addEventListener("click", function (e) {
         projectsSection.classList.add('none');
-    },false);
+    }, false);
 
-    closeContact.addEventListener("click", function(e) {  
+    closeContact.addEventListener("click", function (e) {
         contactSection.classList.add('none');
-    },false);
+    }, false);
 
-    window.addEventListener("load", function(e) {  
+    window.addEventListener("load", function (e) {
         const loader = document.querySelector('.loader');
-        body.removeChild(loader); 
-    },false);
+        body.removeChild(loader);
+    }, false);
 
+    window.addEventListener("resize", function (e) {
+        if (window.innerWidth < 1000) {
+            aboutSection.classList.add('none');
+            projectsSection.classList.add('none');
+            contactSection.classList.add('none');
+        }
+        console.log('resize');
+    }, false);
 })();

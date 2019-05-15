d372cdfb4c2daff1c003901e5285ece7604228a1
MateuszGra <mxgrabowski@gmail.com>
2019-05-15 21:00:52 +0200

    more projects

--- script/main.js
+++ script/main.js
@@ -4,6 +4,10 @@
     const close = document.querySelectorAll('#close');
     const windowTopBar = document.querySelectorAll('.windowTop');
 
+    const backProject = document.querySelector('.backProject');
+    const nextProject = document.querySelector('.nextProject');
+    const wrapper = document.querySelectorAll('#wrapper');
+
     const inputUrl = document.querySelector(".inputUrl");
     inputUrl.value = 'https://mateuszgra.github.io/front-end-portfolio/';
 
@@ -14,7 +18,7 @@
         for (let z = 0; z < icons.length; z++) {
             section[z].style.zIndex = 0;
             windowTopBar[z].classList.add('notActive');
-            if (window.innerWidth < 1000) {
+            if (window.innerWidth < 1025) {
                 section[z].classList.add('none');
             }
         }
@@ -57,5 +61,33 @@
                 section[i].classList.add('none');
             }
         }
+    }, false);
+
+
+    // next project
+    let position = 0;
+
+    afterOrBefore = () => {
+        if (position >= wrapper.length) {
+            position = 0;
+        }
+        if (position < 0) {
+            position = wrapper.length - 1;
+        }
+
+        for (let i = 0; i < wrapper.length; i++) {
+            wrapper[i].classList.add('none');
+        }
+        wrapper[position].classList.remove('none');
+    }
+
+    backProject.addEventListener("click", (e) => {
+        position--
+        afterOrBefore();
+    }, false);
+
+    nextProject.addEventListener("click", (e) => {
+        position++
+        afterOrBefore();
     }, false);
 })();
